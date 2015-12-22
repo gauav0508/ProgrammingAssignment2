6a479be12b4d126d27f1e4cb962071edcc284625
gaurav.jayaswal <gaurav0508@gmail.com>
2015-12-22 18:48:31 -0500

    add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The overall functionality of below two functions is to produce the inverted matrix as an output
+## Cache the output and use it when the matrix is not changing instead of recalculating the inverted 
+## matrix again
 
-## Write a short comment describing this function
+
 ## the function sets value for inverted matrix in cache
-## just for commit
+
 makeCacheMatrix <- function(x = matrix()) {
   invm <- NULL
   
@@ -24,16 +25,18 @@ makeCacheMatrix <- function(x = matrix()) {
   
 }
 
+## Cachesolve function checks if the invm is null. if its not null then retrives the value
+## from cache otherwise it gets the matrix and produce the inverted matrix
 cacheSolve <- function(x) {
   ## Return a matrix that is the inverse of 'x'
   invm <- x$getinvmat()
   if(!is.null(invm)){
-    print("from Cache")
+    print("using Cache value")
     return(invm)
   }
-  data1 <- x$get()
-  invm <- solve(data1) %*% data
-  x$setinvmat(invm)
+  data <- x$get()
+  invm <- solve(data) %*% data ## calculating the inverted matrix
+  x$setinvmat(invm) 
   invm
 }
 
