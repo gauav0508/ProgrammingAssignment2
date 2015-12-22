396cc98e31e1211fdc53bd8d3bde288b19a22a7f
gaurav.jayaswal <gaurav0508@gmail.com>
2015-12-22 16:38:26 -0500

    Function Modified

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## the function sets value for inverted matrix in cache
+## just for commit
 makeCacheMatrix <- function(x = matrix()) {
-
+  invm <- NULL
+  
+  set <- function(y){
+    x <<- y
+    invm <<- NULL
+  }
+  get <- function() x
+  
+  setinvmat <- function(invmat){
+    invm <<- invmat
+  }
+  getinvmat <- function() invm
+  
+  list(set = set, get = get,
+       setinvmat = setinvmat,
+       getinvmat = getinvmat)
+  
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  ## Return a matrix that is the inverse of 'x'
+  invm <- x$getinvmat()
+  if(!is.null(invm)){
+    print("from Cache")
+    return(invm)
+  }
+  data1 <- x$get()
+  invm <- solve(data1) %*% data
+  x$setinvmat(invm)
+  invm
 }
+
